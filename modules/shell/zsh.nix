f1127cc4d5ce2aaecf1232280fb6f070b77c69a2
julian-irizarry <77250613+julian-irizarry@users.noreply.github.com>
2026-04-18 21:43:14 -0400

    feat(zsh): add missing bindkeys, color aliases, completion zstyles
    Ported from ~/.zshrc:
    - bindkey '^R' history-incremental-search (restore after oh-my-zsh rebinds it)
    - bindkey '^[[13;2u' autosuggest-accept (pairs with kitty shift+enter)
    - color aliases for grep/diff/ip
    - GTEST_COLOR=1 session variable
    - zstyle ':completion:*' block with expand/complete/correct/approximate,
      group-name, menu select, matcher-list, kill-process highlighting

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -37,11 +37,15 @@
       ls = "eza -a --icons=always";
       ll = "eza -lhag --icons=always";
       cat = "bat";
+      grep = "grep --color=auto";
+      diff = "diff --color=auto";
+      ip = "ip -color=auto";
     };
 
     sessionVariables = {
       EDITOR = "nvim";
       ZSH = "$HOME/.oh-my-zsh";
+      GTEST_COLOR = "1";
     };
 
     initContent = lib.mkMerge [
@@ -49,8 +53,28 @@
         [ -f "$HOME/.zshenv.local" ] && source "$HOME/.zshenv.local"
       '')
       ''
+        bindkey '^R' .history-incremental-search-backward
         bindkey "^[[A" .up-line-or-history
         bindkey "^[[B" .down-line-or-history
+        # Shift+Enter (paired with kitty's send_text all \x1b[13;2u) accepts
+        # the zsh-autosuggestions ghost text without executing.
+        bindkey '^[[13;2u' autosuggest-accept
+
+        # Completion system tweaks.
+        zstyle ':completion:*' auto-description 'specify: %d'
+        zstyle ':completion:*' completer _expand _complete _correct _approximate
+        zstyle ':completion:*' format 'Completing %d'
+        zstyle ':completion:*' group-name ""
+        zstyle ':completion:*' menu select=2
+        zstyle ':completion:*:default' list-colors ''${(s.:.)LS_COLORS}
+        zstyle ':completion:*' list-colors ""
+        zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
+        zstyle ':completion:*' matcher-list "" 'm:{a-z}={A-Z}' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=* l:|=*'
+        zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
+        zstyle ':completion:*' use-compctl false
+        zstyle ':completion:*' verbose true
+        zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
+        zstyle ':completion:*:kill:*' command 'ps -u $USER -o pid,%cpu,tty,cputime,cmd'
 
         autoload -Uz edit-command-line
         zle -N edit-command-line

f369cc48835d27cfd37a31062c4b647e3bc1ba04
julian-irizarry <77250613+julian-irizarry@users.noreply.github.com>
2026-04-18 23:15:18 -0400

    feat(nvim): enable conform format-on-save

--- modules/editors/neovim/lua/configs/conform.lua
+++ modules/editors/neovim/lua/configs/conform.lua
@@ -2,15 +2,13 @@ local options = {
   formatters_by_ft = {
     lua = { "stylua" },
     cpp = { "clang-format" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    c = { "clang-format" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_format = "fallback",
+  },
 }
 
 return options

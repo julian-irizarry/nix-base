8054e3c21a469d469516458f12ec8ca9cf2543b7
Julian Irizarry <julianirizarry@live.com>
2026-04-24 09:39:02 -0400

    feat: add lib/mkSystem.nix — nixosSystem wrapper with optional home-manager

---
+++ lib/mkSystem.nix
@@ -0,0 +1,34 @@
+{
+  nixpkgs,
+  home-manager,
+  homeModulesDefault,
+}:
+
+{
+  system,
+  modules ? [ ],
+  homeModules ? null,
+}:
+
+nixpkgs.lib.nixosSystem {
+  inherit system;
+  modules =
+    [
+      ../nixos
+    ]
+    ++ modules
+    ++ nixpkgs.lib.optionals (homeModules != null) [
+      home-manager.nixosModules.home-manager
+      (
+        { config, ... }:
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.${config.sys.username} = {
+            imports = [ homeModulesDefault ] ++ homeModules;
+            my.platform.nixGL.enable = false;
+          };
+        }
+      )
+    ];
+}

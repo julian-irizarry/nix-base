9dd15fc22002049080f305b12bdde0cfd6439ac2
julian-irizarry <77250613+julian-irizarry@users.noreply.github.com>
2026-05-04 20:46:58 -0400

    feat(neovim): add vim-fugitive

--- home/editors/neovim/lua/plugins/init.lua
+++ home/editors/neovim/lua/plugins/init.lua
@@ -94,6 +94,8 @@ return {
     end,
   },
 
+  { "tpope/vim-fugitive", lazy = false },
+
   {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
